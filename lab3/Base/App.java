fbe602590a345ac96eb979779d9a1e47d7ee3628
drake13371337 <48164943+drake13371337@users.noreply.github.com>
2019-09-30 21:32:03 +0300

    Add files via upload

--- lab3/Base/App.java
+++ lab3/Base/App.java
@@ -1,4 +1,4 @@
-package Application;
+package Base;
 
 import java.util.*;
 import DAOApi.*;
@@ -10,39 +10,42 @@ public class App
     public static void main(final String[] args) throws Exception {
         final CharDB DataBase = new CharDB();
         DataBase.connect();
-        DataBase.create();
+        //DataBase.create();
         
-        DataBase.table.insertChar(create1());
-        DataBase.tableA.insertChar(create2());
+        //DataBase.table.insertChar(create1());
+        //DataBase.tableA.insertChar(create2());
         
         final List<Character> listRes = DataBase.table.getTable();
         for (final Character t : listRes) {
-            System.out.println(String.valueOf(t.name) + " " + t.playTime + "h " + t.level + "lvl " + t.classP + " " + t.ascendancyClass);
+            System.out.println(String.valueOf(t.name) + " " + t.playTime + "h " + t.level + "lvl " + t.classP + " " + t.ascendancy.ascendancyClass);
         }
+        
+        DataBase.disconnect();
     }
     
     public static List<Character> create1() {
         final List<Character> list = new ArrayList<Character>();
-        list.add(new Character("Teon", "Duelist", 8, 1));
-        list.add(new Character("Ferune", "Witch", 89, 23));
-        list.add(new Character("Fineme", "Witch", 56, 25));
-        list.add(new Character("Dan", "Scion", 7, 23));
-        list.add(new Character("Gamart", "Witch", 1, 80));
-        list.add(new Character("Zarol", "Templar", 24, 74));
-        list.add(new Character("Anig", "Ranger", 14, 31));
-        list.add(new Character("Thiale", "Marauder", 20, 58));
-        list.add(new Character("Ralis", "Witch", 17, 56));
-        list.add(new Character("Nizan", "Scion", 14, 61));
-        list.add(new Character("Prinl", "Ranger", 8, 80));
-        list.add(new Character("Onnedebri", "Ranger", 9, 2));
-        list.add(new Character("Xunth", "Templar", 18, 10));
-        list.add(new Character("Veraro", "Shadow", 21, 58));
-        list.add(new Character("Wagonncrw", "Witch", 45, 61));
-        list.add(new Character("Zedieytia", "Duelist", 96, 40));
-        list.add(new Character("Iar", "Templar", 75, 79));
-        list.add(new Character("TerYrkahai", "Witch", 66, 68));
-        list.add(new Character("Var", "Duelist", 51, 80));
-        list.add(new Character("Maloruig", "Shadow", 11, 1));
+        List<Ascendancy> list1 = create2();
+        list.add(new Character(list1.get(0), "Teon", "Duelist", 8, 1));
+        list.add(new Character(list1.get(1), "Ferune", "Witch", 89, 23));
+        list.add(new Character(list1.get(2), "Fineme", "Witch", 56, 25));
+        list.add(new Character(list1.get(3), "Dan", "Scion", 7, 23));
+        list.add(new Character(list1.get(4), "Gamart", "Witch", 1, 80));
+        list.add(new Character(list1.get(5), "Zarol", "Templar", 24, 74));
+        list.add(new Character(list1.get(6), "Anig", "Ranger", 14, 31));
+        list.add(new Character(list1.get(7), "Thiale", "Marauder", 20, 58));
+        list.add(new Character(list1.get(8), "Ralis", "Witch", 17, 56));
+        list.add(new Character(list1.get(9), "Nizan", "Scion", 14, 61));
+        list.add(new Character(list1.get(10), "Prinl", "Ranger", 8, 80));
+        list.add(new Character(list1.get(11), "Onnedebri", "Ranger", 9, 2));
+        list.add(new Character(list1.get(12), "Xunth", "Templar", 18, 10));
+        list.add(new Character(list1.get(13), "Veraro", "Shadow", 21, 58));
+        list.add(new Character(list1.get(14), "Wagonncrw", "Witch", 45, 61));
+        list.add(new Character(list1.get(15), "Zedieytia", "Duelist", 96, 40));
+        list.add(new Character(list1.get(16), "Iar", "Templar", 75, 79));
+        list.add(new Character(list1.get(17), "TerYrkahai", "Witch", 66, 68));
+        list.add(new Character(list1.get(18), "Var", "Duelist", 51, 80));
+        list.add(new Character(list1.get(19), "Maloruig", "Shadow", 11, 1));
         return list;
     }
     
